d8b4cd58162d202c2e4db4161bfcda540148a72e
FloraBajolet <fbajolet@gmail.com>
2022-01-26 18:42:27 +0100

    2022 January 26 - Work on implementing preprocessing of client's data.

--- dashboard.py
+++ dashboard.py
@@ -31,43 +31,11 @@ st.set_page_config(layout="wide")
 
 # Load data from client
 
-uploaded_file = st.file_uploader("Upload 'data_train_clean_select.csv' file...")
-
-#st.text_input("Path to folder containing 'data_train_clean_select.csv'", key="path")
-#st.write("Path: ", st.session_state.path)
-
-
-#path_application = os.path.join(st.session_state.path, "application_train.csv")
-#path_bureau = os.path.join(st.session_state.path, "bureau.csv")
-#path_bureau_bal = os.path.join(st.session_state.path, "bureau_balance.csv")
-#path_credit_card = os.path.join(st.session_state.path, "credit_card_balance.csv")
-#path_installements= os.path.join(st.session_state.path, "installments_payments.csv")
-#path_pos_cash = os.path.join(st.session_state.path, "POS_CASH_balance.csv")
-#path_previous_app = os.path.join(st.session_state.path, "previous_application.csv")
-
-#path_data = os.path.join(st.session_state.path, "data_train_clean_select.csv")
-
-# Add a placeholder
-#latest_iteration = st.empty()
-#bar = st.progress(0)
-
-# Read database of clients
-all_data = pd.read_csv(uploaded_file, encoding="utf-8")
-
-# Drop unecessary columns
-all_data.drop(["Unnamed: 0", "SK_ID_CURR", "TARGET"], axis=1, inplace=True)
-
-# Select one client at random
-rand_client_idx = np.random.randint(0, all_data.shape[0])
-data_client = all_data.loc[rand_client_idx, :]
-data_client_df = pd.DataFrame(data_client).T
-# For display (need 2 lines)
-data_clients = all_data.loc[rand_client_idx:rand_client_idx+1, :]
-data_clients_df = pd.DataFrame(data_clients)
-
-#### MODELING ####
+uploaded_file = st.file_uploader("Upload client Excel file...")
 
 # Get the pickled preprocessor, model, explainer
+pickle_cat_to_num = open("cat_to_num.pickle", "rb")
+cat_to_num = pickle.load(pickle_cat_to_num)
 pickle_preprocessor = open("scalers_preprocessing.pickle", "rb")
 scalers_preprocessing = pickle.load(pickle_preprocessor)
 pickle_model = open("best_model_lr.pickle", "rb")
@@ -77,101 +45,139 @@ kernel_explainer = pickle.load(pickle_explainer)
 pickle_feature_names = open("feature_names.pickle", "rb")
 feature_names = pickle.load(pickle_feature_names)
 
+
+# Read database of clients
+application = pd.read_excel(uploaded_file, sheet_name="application")
+bureau = pd.read_excel(uploaded_file, sheet_name="bureau")
+bureau_balance = pd.read_excel(uploaded_file, sheet_name="bureau_balance")
+installments = pd.read_excel(uploaded_file, sheet_name="installments")
+pos_cash = pd.read_excel(uploaded_file, sheet_name="pos_cash")
+previous_app = pd.read_excel(uploaded_file, sheet_name="previous_app")
+
+
+# Drop unecessary columns
+application.drop(["SK_ID_CURR", "TARGET"], axis=1, inplace=True)
+
+# to drop "AMT_GOODS_PRICE", "CNT_FAM_MEMBERS",
+
+#### TRANSFORM CATEGORICAL VARIABLES TO NUMERICAL ####
+
+
+# Select one client at random
+#rand_client_idx = np.random.randint(0, all_data.shape[0])
+#data_client = all_data.loc[rand_client_idx, :]
+#data_client_df = pd.DataFrame(data_client).T
+# For display (need 2 lines)
+#data_clients = all_data.loc[rand_client_idx:rand_client_idx+1, :]
+#data_clients_df = pd.DataFrame(data_clients)
+
+#### MODELING ####
+
+
 # Transform client's data
-data_client_transformed = scalers_preprocessing.transform(data_client_df)
-data_client_transformed = pd.DataFrame(data_client_transformed)
+#data_client_transformed = scalers_preprocessing.transform(data_client_df)
+#data_client_transformed = pd.DataFrame(data_client_transformed)
 
 # Predict
-prediction = best_model_lr.predict_proba(data_client_transformed)
-prediction_default = prediction[0][1]*100
+#prediction = best_model_lr.predict_proba(data_client_transformed)
+#prediction_default = prediction[0][1]*100
 
 # Explain features
-explained_sample = kernel_explainer.shap_values(data_client_transformed.loc[0, :])
+#explained_sample = kernel_explainer.shap_values(data_client_transformed.loc[0, :])
 # Format shap values
-shap_explained, most_important_features = format_shap_values(explained_sample, feature_names)
-#most_important_features = shap_explained["features"].tolist()
+#shap_explained, most_important_features = format_shap_values(explained_sample, feature_names)
 
 #### DISTRIBUTION PLOTS ####
 # Default feature
-num_features = list(all_data.select_dtypes(include=["float64", "int64"]).columns)
+#num_features = list(all_data.select_dtypes(include=["float64", "int64"]).columns)
 
 
 ############## DISPLAY ##############
 
-# Option show full dataframe
-if st.checkbox('Show full dataframe'):
-    all_data
+
+# Option show data
+if st.checkbox('Show input client data'):
+    st.write("APPLICATION data")
+    st.dataframe(application)
+    st.write("BUREAU data")
+    st.dataframe(bureau)
+    st.write("BUREAU BALANCE data")
+    st.dataframe(bureau_balance)
+    st.write("PREVIOUS APPLICATION data")
+    st.dataframe(previous_app)
+    st.write("INSTALLEMENTS data")
+    st.dataframe(installments)
+    st.write("POS_CASH data")
+    st.dataframe(pos_cash)
 
 # Display clients data and prediction
-st.write(f"Client #{rand_client_idx}")
-st.dataframe(data_clients_df)
+#st.write(f"Client #{rand_client_idx}")
+#st.dataframe(data_clients_df)
 
-st.write(f"Client #{rand_client_idx} has {prediction_default:.1f} % of risk to make default.")
+#st.write(f"Client #{rand_client_idx} has {prediction_default:.1f} % of risk to make default.")
 
-if prediction_default < 40:
-    st.write(f"We recommand to accept client's application to loan.")
-elif (prediction_default >= 40) & (prediction_default <= 60):
-    st.write(f"Client's chances to make default are close to 50/50. We recommand to analyse closely the data to make your decision.")
-else:
-    st.write(f"We recommand to reject client's application to loan.")
+#if prediction_default < 40:
+#    st.write(f"We recommand to accept client's application to loan.")
+#elif (prediction_default >= 40) & (prediction_default <= 60):
+#    st.write(f"Client's chances to make default are close to 50/50. We recommand to analyse closely the data to make your decision.")
+#else:
+#    st.write(f"We recommand to reject client's application to loan.")
 
-left_column, right_column = st.columns([3, 2])
+#left_column, right_column = st.columns([3, 2])
 
-with left_column:
+#with left_column:
 
     # Bokeh plot explained features
-    explained_plot = figure(y_range=most_important_features, title="Most important data in the algorithm decision")
+    #explained_plot = figure(y_range=most_important_features, title="Most important data in the algorithm decision")
 
-    source = ColumnDataSource(data=shap_explained)
-    bars = explained_plot.hbar(y="features", left="left", right="right", height=0.5, color="color", 
-                                hover_line_color="black", hover_line_width=2, source=source)
+    #source = ColumnDataSource(data=shap_explained)
+    #bars = explained_plot.hbar(y="features", left="left", right="right", height=0.5, color="color", 
+    #                            hover_line_color="black", hover_line_width=2, source=source)
 
-    explained_plot.xaxis.axis_label = "Impact on model output"
-    explained_plot.yaxis.axis_label = "Client's informations"
-    explained_plot.add_tools(HoverTool(tooltips=[("Importance", "@shap_values")], 
-                                renderers = [bars]))
+    #explained_plot.xaxis.axis_label = "Impact on model output"
+    #explained_plot.yaxis.axis_label = "Client's informations"
+    #explained_plot.add_tools(HoverTool(tooltips=[("Importance", "@shap_values")], 
+    #                            renderers = [bars]))
 
-    st.bokeh_chart(explained_plot)
+    #st.bokeh_chart(explained_plot)
 
-with right_column:
-    feature_distrib = st.selectbox(
-        "Variable distribution to show",
-        num_features)
+#with right_column:
+    #feature_distrib = st.selectbox(
+    #    "Variable distribution to show",
+    #    num_features)
     
     # Get value for client
-    data_client_value = data_client_df.loc[data_client_df.index[0], feature_distrib]
+    #data_client_value = data_client_df.loc[data_client_df.index[0], feature_distrib]
     # Generate distribution data
-    hist, edges = np.histogram(all_data[feature_distrib], bins=50)
-    hist_source_df = pd.DataFrame({"edges_left": edges[:-1], "edges_right": edges[1:], "hist":hist})
-    max_histogram = hist_source_df["hist"].max()
-    client_line = pd.DataFrame({"x": [data_client_value, data_client_value],
-                                "y": [0, max_histogram]})
-    hist_source = ColumnDataSource(data=hist_source_df)
+    #hist, edges = np.histogram(all_data[feature_distrib], bins=50)
+    #hist_source_df = pd.DataFrame({"edges_left": edges[:-1], "edges_right": edges[1:], "hist":hist})
+    #max_histogram = hist_source_df["hist"].max()
+    #client_line = pd.DataFrame({"x": [data_client_value, data_client_value],
+    #                            "y": [0, max_histogram]})
+    #hist_source = ColumnDataSource(data=hist_source_df)
 
     # Make figure
-    p = figure(title=f"Client value for {feature_distrib} compared to other clients", plot_width=400, plot_height=500)
-    qr = p.quad(top="hist", bottom=0, line_color="white", left="edges_left", right="edges_right",
-        fill_color="navy", hover_fill_color="orange", legend_label="Number of clients", 
-        alpha=0.5, hover_alpha=1, source=hist_source)
+    #p = figure(title=f"Client value for {feature_distrib} compared to other clients", plot_width=400, plot_height=500)
+    #qr = p.quad(top="hist", bottom=0, line_color="white", left="edges_left", right="edges_right",
+    #    fill_color="navy", hover_fill_color="orange", alpha=0.5, hover_alpha=1, source=hist_source)
 
-    p.line(x=client_line["x"], y=client_line["y"], line_color="orange", line_width=2, line_dash="dashed")
+    #p.line(x=client_line["x"], y=client_line["y"], line_color="orange", line_width=2, line_dash="dashed")
 
-    label_client = Label(text="Value for client", x=data_client_value, y=max_histogram, text_color="orange",
-                        x_offset=-50, y_offset=10)
-    hover_tools = HoverTool(tooltips=[("Between:", "@edges_left"), ("and:", "@edges_right"), ("Count:", "@hist")], 
-                        renderers = [qr])
+    #label_client = Label(text="Value for client", x=data_client_value, y=max_histogram, text_color="orange",
+    #                    x_offset=-10, y_offset=10)
+    #hover_tools = HoverTool(tooltips=[("Between:", "@edges_left"), ("and:", "@edges_right"), ("Count:", "@hist")], 
+    #                    renderers = [qr])
 
-    p.xaxis.axis_label = feature_distrib
-    p.y_range.start = 0
-    p.y_range.range_padding = 0.2
-    p.legend.location = "top_right"
-    p.yaxis.axis_label = "Count"
-    p.grid.grid_line_color="grey"
-    p.xgrid.grid_line_color=None
-    p.ygrid.grid_line_alpha=0.5
+    #p.xaxis.axis_label = feature_distrib
+    #p.y_range.start = 0
+    #p.y_range.range_padding = 0.2
+    #p.yaxis.axis_label = "Number of clients"
+    #p.grid.grid_line_color="grey"
+    #p.xgrid.grid_line_color=None
+    #p.ygrid.grid_line_alpha=0.5
 
-    p.add_tools(hover_tools)
-    p.add_layout(label_client)
+    #p.add_tools(hover_tools)
+    #p.add_layout(label_client)
 
 
-    st.bokeh_chart(p)
+    #st.bokeh_chart(p)
--- dashboard_functions.py
+++ dashboard_functions.py
@@ -11,6 +11,266 @@ import sys
 from sklearn.preprocessing import StandardScaler
 
 
+## PREPROCESSING ##
+def cat_to_num(bureau, bureau_balance, previous_app, pos_cash, cat_to_num):
+    """
+    Replace categorical features by ordinal or numerical ones using provided dictionnary for mapping.
+    Return processed dataframes.
+
+    bureau, bureau_balance, previous_app, pos_cash: dataframes.
+    cat_to_num: dict of nested dict with numerical values to replace categorical classes.
+    """
+    bureau_all_num = bureau.copy()
+    bureau_balance_all_num = bureau_balance.copy()
+    previous_app_all_num = previous_app.copy()
+    pos_cash_all_num = pos_cash.copy()
+
+    # Bureau
+    features = ["CREDIT_ACTIVE", "CREDIT_CURRENCY", "CREDIT_TYPE"]
+    scores = [cat_to_num["categ_b_credit_active"],
+            cat_to_num["categ_b_currency"], 
+            cat_to_num["score_credit_type"]]
+
+    for feature, score in zip(features, scores):
+        bureau_all_num[feature] = bureau_all_num[feature].map(score)
+    
+    # Bureau balance
+    bureau_balance_all_num["STATUS"] = bureau_balance_all_num["STATUS"].map(cat_to_num["categ_bb_status"])
+
+    #Previous app
+    features = ["NAME_CONTRACT_TYPE", 
+                "NAME_CONTRACT_STATUS", 
+                "NAME_YIELD_GROUP", 
+                "NAME_PRODUCT_TYPE", 
+                "FLAG_LAST_APPL_PER_CONTRACT", 
+                "CODE_REJECT_REASON", 
+                "NAME_CASH_LOAN_PURPOSE", 
+                "PRODUCT_COMBINATION", 
+                "CHANNEL_TYPE", 
+                "NAME_SELLER_INDUSTRY", 
+                "NAME_GOODS_CATEGORY"]
+    scores = [cat_to_num["categ_prev_name_contract_type"], 
+            cat_to_num["categ_prev_name_contract_status"],
+            cat_to_num["categ_prev_name_yield_group"], 
+            cat_to_num["categ_prev_name_product_type"], 
+            cat_to_num["categ_prev_flag_last_appl_per_contract"],
+            cat_to_num["score_code_reject_reason"],
+            cat_to_num["score_name_cash_loan_purpose"],
+            cat_to_num["score_product_combination"],
+            cat_to_num["score_channel_type"],
+            cat_to_num["score_name_seller_industry"],
+            cat_to_num["score_name_goods_category"]]
+
+    # Replace classes by ordinal or numerical value
+    for feature, score in zip(features, scores):
+        previous_app_all_num[feature] = previous_app_all_num[feature].map(score)
+
+    # Drop categorical columns with little influence on default risk
+    previous_app_all_num.drop(["WEEKDAY_APPR_PROCESS_START", "NAME_PAYMENT_TYPE", 
+                                "NAME_TYPE_SUITE", "NAME_CLIENT_TYPE", "NAME_PORTFOLIO"], 
+                                axis=1, inplace=True)
+
+    # Pos_cash
+    pos_cash_all_num["NAME_CONTRACT_STATUS"] = pos_cash_all_num["NAME_CONTRACT_STATUS"] \
+                                                .map(cat_to_num["categ_pos_name_contract_status"])
+
+    return bureau_all_num, bureau_balance_all_num, previous_app_all_num, pos_cash_all_num
+
+
+def application_train_agg(df, verbose=False):
+    """
+    Create new features on the application dataframe and replace DAYS_EMPLOYED by NaN if needed.
+    Return dataframe with new features.
+    Optional: verbose to print info for debug.
+    """
+
+    df_new = df.copy()
+
+    # Replace NaN values for DAYS_EMPLOYED: 365.243 -> nan
+    df_new['DAYS_EMPLOYED'].replace(365243, np.nan, inplace= True)
+    # Some simple new features (percentages)
+    df_new['DAYS_EMPLOYED_PERC'] = df_new['DAYS_EMPLOYED'] / df_new['DAYS_BIRTH']
+    df_new['INCOME_CREDIT_PERC'] = df_new['AMT_INCOME_TOTAL'] / df_new['AMT_CREDIT']
+    df_new['INCOME_PER_PERSON'] = df_new['AMT_INCOME_TOTAL'] / df_new['CNT_FAM_MEMBERS']
+    df_new['ANNUITY_INCOME_PERC'] = df_new['AMT_ANNUITY'] / df_new['AMT_INCOME_TOTAL']
+    df_new['PAYMENT_RATE'] = df_new['AMT_ANNUITY'] / df_new['AMT_CREDIT']
+    
+    if verbose:
+        print(f"FINAL SHAPE: n_rows: {df_new.shape[0]} / n_columns: {df_new.shape[1]}.")
+
+    return df_new
+
+
+def bureau_and_balance_agg(df_bureau, df_bureau_balance, verbose=False):
+    """
+    Aggregate bureau and bureau_balance data with feature engineering.
+    Return new dataframe.
+    """
+
+    df_bureau_new = df_bureau.copy()
+    df_bureau_balance_new = df_bureau_balance.copy()
+    
+    # Bureau balance: Perform aggregations and merge with bureau
+    bb_aggregations = {'MONTHS_BALANCE': ['min', 'max', 'size'],
+                      "STATUS": ["mean"]}
+
+    if verbose:
+        print("Aggregate bureau_balance...")
+    bb_agg = df_bureau_balance_new.groupby('SK_ID_BUREAU').agg(bb_aggregations)
+    bb_agg.columns = pd.Index([e[0] + "_" + e[1].upper() for e in bb_agg.columns.tolist()])
+    
+    if verbose:
+        print("Join bureau and bureau_balance...")
+    df_bureau_new = df_bureau_new.join(bb_agg, how='left', on='SK_ID_BUREAU')
+    
+    # Bureau and bureau_balance numeric features
+    num_aggregations = {
+        'DAYS_CREDIT': ['min', 'max', 'mean', 'var'],
+        'DAYS_CREDIT_ENDDATE': ['max', 'mean'],
+        'CREDIT_DAY_OVERDUE': ['max'],
+        'AMT_CREDIT_MAX_OVERDUE': ['mean'],
+        'AMT_CREDIT_SUM': ['max'],
+        'AMT_CREDIT_SUM_DEBT': ['mean'],
+        'AMT_CREDIT_SUM_OVERDUE': ['mean'],
+        'AMT_CREDIT_SUM_LIMIT': ['mean', 'sum'],
+        'AMT_ANNUITY': ['max', 'mean'],
+        'CNT_CREDIT_PROLONG': ['sum'],
+        'MONTHS_BALANCE_MIN': ['min'],
+        'MONTHS_BALANCE_MAX': ['max'],
+        'MONTHS_BALANCE_SIZE': ['mean', 'sum']
+    }
+    
+    # Categorical (ordinal) aggregations
+    cat_aggregations = {
+        'CREDIT_ACTIVE': ['mean', 'sum'],
+        'CREDIT_CURRENCY': ['mean'],
+        'CREDIT_TYPE': ['mean', 'max']
+    }
+    
+    if verbose:
+        print("Aggregate bureau...")
+    bureau_agg = df_bureau_new.groupby('SK_ID_CURR').agg({**num_aggregations, **cat_aggregations})
+    bureau_agg.columns = pd.Index(['BUREAU_' + e[0] + "_" + e[1].upper() for e in bureau_agg.columns.tolist()])
+
+    return bureau_agg
+
+
+
+
+def previous_applications_agg(df, verbose=False):
+    """
+    Replace false values by NaN.
+    Aggregate previous applications dataframe with feature engineering.
+    Return new dataframe.
+    """
+    df_prev_app = df.copy()
+
+    if verbose:
+        print("Feature engineering...")
+    # Days 365.243 values -> nan
+    df_prev_app['DAYS_FIRST_DRAWING'].replace(365243, np.nan, inplace= True)
+    df_prev_app['DAYS_FIRST_DUE'].replace(365243, np.nan, inplace= True)
+    df_prev_app['DAYS_LAST_DUE_1ST_VERSION'].replace(365243, np.nan, inplace= True)
+    df_prev_app['DAYS_LAST_DUE'].replace(365243, np.nan, inplace= True)
+    df_prev_app['DAYS_TERMINATION'].replace(365243, np.nan, inplace= True)
+    # Add feature: value ask / value received percentage
+    df_prev_app['APP_CREDIT_PERC'] = df_prev_app['AMT_APPLICATION'] / df_prev_app['AMT_CREDIT']
+    # Previous applications numeric features
+    num_aggregations = {
+        'AMT_ANNUITY': ['min', 'max', 'mean'],
+        'AMT_APPLICATION': ['min'],
+        'AMT_CREDIT': ['min'],
+        'APP_CREDIT_PERC': ['min', 'max', 'mean', 'var'],
+        'AMT_DOWN_PAYMENT': ['min','mean'],
+        'AMT_GOODS_PRICE': ['min'],
+        'HOUR_APPR_PROCESS_START': ['min', 'max', 'mean'],
+        'DAYS_DECISION': ['min', 'max'],
+        'CNT_PAYMENT': ['mean'],
+    }
+    # Previous applications categorical features
+    cat_aggregations = {
+        'FLAG_LAST_APPL_PER_CONTRACT': ['mean'],
+        'NAME_CASH_LOAN_PURPOSE': ['mean'],
+        'NAME_CONTRACT_STATUS': ['max'],
+        'CODE_REJECT_REASON': ['mean'],
+        'NAME_YIELD_GROUP': ['mean'],
+        'PRODUCT_COMBINATION': ['mean'],
+    }
+
+    if verbose:
+        print("Aggregation of features...")
+    prev_agg = df_prev_app.groupby('SK_ID_CURR').agg({**num_aggregations, **cat_aggregations})
+    prev_agg.columns = pd.Index(['PREV_' + e[0] + "_" + e[1].upper() for e in prev_agg.columns.tolist()])
+
+    return prev_agg
+
+def pos_cash_agg(df):
+    """
+    Aggregate pos_cash dataframe with feature engineering.
+    Return new dataframe.
+    """
+
+    df_pos_cash = df.copy()
+
+    #df_pos_cash.drop(["CNT_INSTALMENT", "CNT_INSTALMENT_FUTURE", "SK_ID_PREV"], axis=1, inplace=True)
+
+    # Numerical features aggregation
+    num_aggregations = {
+        'MONTHS_BALANCE': ['max', 'mean'],
+        'SK_DPD': ['max'],
+        'SK_DPD_DEF': ['max']
+    }
+
+    # categorical feature aggregation
+    cat_aggregations = {
+        'NAME_CONTRACT_STATUS': ['min', 'max']
+    }
+    
+    pos_agg = df_pos_cash.groupby('SK_ID_CURR').agg({**num_aggregations, **cat_aggregations})
+    pos_agg.columns = pd.Index(['POS_' + e[0] + "_" + e[1].upper() for e in pos_agg.columns.tolist()])
+    
+    return pos_agg
+
+
+# Preprocess df_installtallments_payments.csv
+def installments_agg(df, verbose=False):
+    """
+    Aggregate installments dataframe with feature engineering.
+    Return new dataframe.
+    """
+    df_install = df.copy()
+
+    if verbose:
+        print("Feature engineering...")
+    # Percentage and difference paid in each df_installtallment (amount paid and df_installtallment value)
+    df_install['PAYMENT_PERC'] = df_install['AMT_PAYMENT'] / df_install['AMT_INSTALMENT']
+    df_install['PAYMENT_DIFF'] = df_install['AMT_INSTALMENT'] - df_install['AMT_PAYMENT']
+    # Days past due and days before due (no negative values)
+    df_install['DBD'] = df_install['DAYS_INSTALMENT'] - df_install['DAYS_ENTRY_PAYMENT']
+    df_install['DBD'] = df_install['DBD'].apply(lambda x: x if x > 0 else 0)
+
+    if verbose:
+        print("Aggregation...")
+    # Features: Perform aggregations
+    num_aggregations = {
+        'NUM_INSTALMENT_VERSION': ['nunique'],
+        'DBD': ['max', 'mean', 'sum'],
+        'PAYMENT_PERC': ['max'],
+        'PAYMENT_DIFF': ['mean'],
+        'AMT_INSTALMENT': ['max'],
+        'AMT_PAYMENT': ['min'],
+        'DAYS_ENTRY_PAYMENT': ['max']
+    }
+
+    df_install_agg = df_install.groupby('SK_ID_CURR').agg(num_aggregations)
+    df_install_agg.columns = pd.Index(['INSTAL_' + e[0] + "_" + e[1].upper() for e in df_install_agg.columns.tolist()])
+
+    return df_install_agg
+
+
+## MODELING ##
+
+
 def format_shap_values(shap_values, feature_names):
     """
     Format shap values into a dataframe to be plotted with Bokeh.
